2e09709284a171750d642e5127a6472d4b20775c
Kerman <kermankohli@gmail.com>
2018-05-01 08:22:49 +1000

    Boilerplate done

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 
 /* Home component */
-import Home from './pages/home/'
+import Home from './pages/home/';
+import styles from './styles/global.scss';
 
 /* App component */
 class App extends Component {
   render() {
     return (
-      <div>
+      <div class="container">
         <nav>
           <ul>
             <li><Link to="/">Homes</Link></li>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,9 +6,9 @@ import styles from './style.scss';
 class Home extends Component {
    render() {
      return (
-       <div>
-         <h2>Home</h2>
-         <p class="test">This is my homepage</p>
+       <div class="section">
+         <h1 class="heading">8x Protocol</h1>
+         <p>Decentralised recurring payments on the Ethereum blockchain</p>
        </div>
      )
    }
