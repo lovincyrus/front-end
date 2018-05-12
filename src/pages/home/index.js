0b6f94b520c0468e8c6cf1625edd147ece23db65
Kerman <kermankohli@gmail.com>
2018-05-12 15:11:52 +1000

    Why section added

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -15,11 +15,17 @@ import businessIcon from '../../assets/images/business.png';
 import userIcon from '../../assets/images/user.png';
 import tokenHoldersIcon from '../../assets/images/token-holders.png';
 
-import broadcastIcon from '../../assets/images/broadcast.png';
-import claimIcon from '../../assets/images/flag.png';
-import executionIcon from '../../assets/images/time.png';
-
 import memphisBackground from '../../assets/images/memphis-colorful.png';
+
+import card from '../../assets/images/card.png'
+import coins from '../../assets/images/coins.png'
+import cash from '../../assets/images/cash.png'
+
+import overview from '../../assets/images/overview.png';
+
+import desk from '../../assets/images/desk.png';
+import platform from '../../assets/images/platform.png';
+import chip from '../../assets/images/chip.png';
 
 class Home extends Component {
    render() {
@@ -46,11 +52,14 @@ class Home extends Component {
 
                 <hr/>
 
-                <div class="what-is-8x section">
+                <div class="what section">
 
                     <div class="container">
 
-                        <h1>What is 8x?</h1>
+                        <div class="header-container">
+                            <img src={coins}/>
+                            <h1>What is 8x?</h1>
+                        </div>
                         <h5>
                             8x is a protocol for facilitating recurring crypto payments without exposing consumers or businesses to the volatility of crypto currencies. Gartner estimates the subscription economy will be worth $3.3T by 2020 with 80% of software products & services moving to a subscription model.
                         </h5>
@@ -61,13 +70,79 @@ class Home extends Component {
 
                 <hr/>
 
-                <div class="ecosystem section-alt">
+                 <div class="how section">
 
                     <div class="container">
 
-                        <div class="heading-container">
-                            <h2>Ecosystem</h2>
-                            <p>The 8x protocol requires an ecosystem of businesses, consumers and token holders in order to operate. </p>
+                        <div class="header-container">
+                            <img src={cash}/>
+                            <h1 class="center-text">How does it work?</h1>
+                        </div>
+
+                        <h5 class="description">
+                            The following flow is shown once a user has given the protocol permission to transfer stable coins on their behalf. 1 stable coin = $1 USD.
+                        </h5>
+
+                        <img class="overview-image" src={overview}/>
+
+                    </div>
+
+                </div>
+
+                <hr/>
+
+                <div class="why section">
+
+                    <div class="container">
+
+                        <div class="header-container">
+                            <h1 class="center-text">Why the 8x token?</h1>
+                        </div>
+
+                        <h5 class="description">
+                            Businesses and users have no utility with the 8x token<br/>as it is only required to operate a service node in the network.
+                        </h5>
+
+                        <div class="reasons-container">
+
+                            <div class="reason">
+                                <img class="icon" src={desk}/>
+                                <p>
+                                    8x tokens are required to be staked in order to run service nodes. 
+                                    This in turns allows token holders to earn up to 1% of every transaction executed by them. The larger the transaction, the more 8x tokens are required.
+                                </p>
+                            </div>
+
+                            <div class="reason">
+                                <img class="icon" src={platform}/>
+                                <p>
+                                    If service nodes claim the right to execute a transaction 20 days before a monthly payment is due, they earn 1/3 of 1%. 
+                                    The remaining 2/3 of the 1% fee are used to purchase 8x tokens on the market and then burned.
+                                </p>
+                            </div>
+
+                            <div class="reason">
+                                <img class="icon" src={chip}/>
+                                <p>
+                                    Governance rights. Any major changes to the protocol may be voted on based on the number of tokens held.
+                                </p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <hr/>
+
+                <div class="ecosystem section">
+
+                    <div class="container">
+
+                        <div class="header-container">
+                            <h1 class="center-text">Ecosystem</h1>
+                            <h5>The 8x protocol requires an ecosystem of businesses, consumers and token holders in order to operate. </h5>
                         </div>
 
                         <div class="items">
@@ -75,43 +150,6 @@ class Home extends Component {
                             <EcosystemBox header={businessIcon} tick={redCheckmark} title="Businesses" features={["Zero volatility through the use of Dai stable coin", "Cheaper than Stripe & PayPal", "Easy to implement SDKs"]}/>
                             <EcosystemBox header={userIcon} tick={yellowCheckmark} title="Users" features={["Zero volatility through the use of Dai stable coin", "Manage all your subscriptions through one interface", "Your funds always stay with you, in your control"]}/>
                             <EcosystemBox header={tokenHoldersIcon} tick={greenCheckmark} title="Token Holders" features={["Earn 8x tokens by processing transactions on the network", "Have a say in what changes are made to the protocol"]}/>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div class="protocol section" style ={ { backgroundImage: "url("+memphisBackground+")" } }>
-
-                    <div class="container">
-
-                        <h2>8x Protocol</h2>
-                        <p>
-                            1. As a business, if you were to accept Ethereum for recurring payments, the volatility would be unfeasible to accept as a payment method. Between January 2018 and March 2018 the price of Ether dropped by 70%. This level of volatility can be infeasible for both businesses and customers. By using the MakerDao stable coin, the protocol allows for stable exchanges of value for both parties.
-                            <br/>
-                            <br/>
-                            2. To enable “pull” payments (bypass the need to manually sign a transaction), we simply ask for your approval to spend Dai on your behalf. Your funds stay with you at all times, not stored in a smart contract.
-                            <br/>
-                            <br/>
-                            3. You can’t schedule repeating transactions on the blockchain automatically. Existing methodologies involve relying on a centralised 3rd party server to schedule transactions. The 8x protocol overcomes this by paying token holders a fee of the transaction to execute the transaction.
-                        </p>
-
-                    </div>
-
-                </div>
-
-                <div class="token-holders section-alt">
-
-                    <div class="container">
-
-                        <h2>Token Holders</h2>
-
-                        <div class="items">
-
-                            <TokenHoldersBox icon={broadcastIcon} title="Initial Transaction" description="When a customer subscribes to a plan, the transaction is emitted to token holders. Token holders are given the option to claim the right to process the transaction. 8x tokens need to be staked in order to claim."/>
-                            <TokenHoldersBox icon={claimIcon} title="Claim Period" description="The earlier a transaction is claimed the less token holders earn in fees. If the claim period is open for 10 days, transactions claimed on the 1st day would earn 10% of the 1% fee. The resulting 90% results in token burn."/>
-                            <TokenHoldersBox icon={executionIcon} title="Execution Time" description="When the subscription is to be paid, claimants are required to call the 8x protocol and execute the transaction. Failing to do so will result in their stake of 8x tokens to be burned. This will reduce the total supply of tokens."/>
 
                         </div>
 
