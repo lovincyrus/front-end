eec80b6b736e06418d7ffdeaccb2c658b087b528
Kerman <kermankohli@gmail.com>
2018-05-19 18:26:41 +1000

    Updated footer + added more links

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -10,9 +10,29 @@ class Footer extends Component {
    render() {
         return (
             <div class="navigation-footer-container">
-                <nav class="container">
+
+                <nav class="logo-container">
                     <img src={logo}/>
                 </nav>
+
+                <div class="sections-container">
+
+                    <div class="footer-section">
+                        <h4>Community</h4>
+                        <a style={{color: '#7D94DF'}} href="https://t.me/joinchat/AXlpZ1LUmBqu3ySXHVh8qA">Telegram</a>
+                        <a style={{color: '#000000'}} href="https://medium.com/8x-protocol">Medium</a>
+                        <a style={{color: '#1EABF5'}} href="https://twitter.com/8xProtocol">Twitter</a>
+                        <a style={{color: '#FD4E02'}} href="https://www.youtube.com/playlist?list=PLdm5dK5Ob2TnSUKlcYj3XfCO-iS8SeEfb">Youtube</a>
+                    </div>
+
+                    <div class="footer-section">
+                        <h4>Foundation</h4>
+                        <a style={{color: '#95959A'}}>Whitepaper</a>
+                        <a style={{color: '#95959A'}}>Github</a>
+                    </div>
+
+                </div>
+
             </div>
         )
    }
--- src/components/mobile-menu/index.js
+++ src/components/mobile-menu/index.js
@@ -10,6 +10,7 @@ export default ({ close }) => (
       <li onClick={close}><a targe="_blank" href="https://docs.google.com/viewer?url=https://github.com/8x-protocol/whitepaper/raw/master/8x_protocol_whitepaper_v4.pdf">Whitepaper</a></li>
       <li onClick={close}><a targe="_blank" href="https://github.com/8x-protocol/">Github</a></li>
       <li onClick={close}><a targe="_blank" href="https://t.me/joinchat/AXlpZ1LUmBqu3ySXHVh8qA">Telegram</a></li>
+      <li onClick={close}><a targe="_blank" href="https://www.youtube.com/playlist?list=PLdm5dK5Ob2TnSUKlcYj3XfCO-iS8SeEfb">Daily Vlogs</a></li>
     </ul>
   </div>
 );
