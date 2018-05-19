2b9d0658c4fc7028bbd6d67279e47c4523519edc
Kerman <kermankohli@gmail.com>
2018-05-19 21:56:05 +1000

    Update twitter handle

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -21,7 +21,7 @@ class Footer extends Component {
                         <h4>Community</h4>
                         <a style={{color: '#7D94DF'}} href="https://t.me/joinchat/AXlpZ1LUmBqu3ySXHVh8qA">Telegram</a>
                         <a style={{color: '#000000'}} href="https://medium.com/8x-protocol">Medium</a>
-                        <a style={{color: '#1EABF5'}} href="https://twitter.com/8xProtocol">Twitter</a>
+                        <a style={{color: '#1EABF5'}} href="https://twitter.com/8x_protocol">Twitter</a>
                         <a style={{color: '#FD4E02'}} href="https://www.youtube.com/playlist?list=PLdm5dK5Ob2TnSUKlcYj3XfCO-iS8SeEfb">Youtube</a>
                     </div>
 
