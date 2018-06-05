8582cd1f33ceca68d9aaaad8aa7f16db24cb9a7c
travisdmathis <travis@grillwork.io>
2018-06-05 08:00:51 -0400

    fix attempt for react-router direct routes in production

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 
 // main app
 import App from './app.js';
 
-ReactDOM.render((
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
-), document.getElementById('app'));
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<App/>, document.getElementById('app'));
+})
