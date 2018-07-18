73d120de164520698530b4fac069241867ef6cef
Kerman <kermankohli@gmail.com>
2018-07-18 16:01:56 +1000

    Cleanup images

--- src/middleware/images.js
+++ src/middleware/images.js
@@ -1,5 +1,4 @@
 import andy from '../assets/images/andy.jpg';
-import businessIcon from '../assets/images/business.png';
 import card from '../assets/images/card.png';
 import cash from '../assets/images/cash.png';
 import chip from '../assets/images/chip.png';
@@ -8,33 +7,20 @@ import daniel from '../assets/images/daniel.jpg';
 import desk from '../assets/images/desk.png';
 import diagram from '../assets/images/8x-diagram.png';
 import github from '../assets/images/github.png';
-import greenCheckmark from '../assets/images/green-checkmark.png';
-import kamal from '../assets/images/kamal.jpg';
 import kerman from '../assets/images/kerman.jpg';
 import kevinL from '../assets/images/kevinl.jpg';
 import kevinZ from '../assets/images/kevinz.jpg';
 import medium from '../assets/images/medium.png';
-import memphisBackground from '../assets/images/memphis-colorful.png';
-import overview from '../assets/images/overview.png';
 import platform from '../assets/images/platform.png';
-import redCheckmark from '../assets/images/red-checkmark.png';
 import reddit from '../assets/images/reddit.png';
-import renee from '../assets/images/renee.jpg';
 import rob from '../assets/images/rob.jpg';
 import subscriptionsiPhone from '../assets/images/subscriptions-iphone.png';
-import subscriptionsPreview from '../assets/images/subscriptions-preview.png';
-import teamLogo from '../assets/images/8x-team.png';
 import telegram from '../assets/images/telegram.png';
-import tokenHoldersIcon from '../assets/images/token-holders.png';
-import travis from '../assets/images/travis.png';
 import twitter from '../assets/images/twitter.png';
-import userIcon from '../assets/images/user.png';
-import yellowCheckmark from '../assets/images/yellow-checkmark.png';
 import waves from '../assets/images/waves.png';
 
 export default {
   andy,
-  businessIcon,
   card,
   cash,
   chip,
@@ -43,27 +29,17 @@ export default {
   desk,
   diagram,
   github,
-  greenCheckmark,
   kamal,
   kerman,
   kevinL,
   kevinZ,
   medium,
-  memphisBackground,
   overview,
-  platform,
-  redCheckmark,
   reddit,
-  renee,
   rob,
   subscriptionsiPhone,
-  subscriptionsPreview,
   teamLogo,
   telegram,
-  tokenHoldersIcon,
-  travis,
   twitter,
-  userIcon,
-  waves,
-  yellowCheckmark
+  waves
 };
