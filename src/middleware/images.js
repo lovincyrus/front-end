463093ad750a500a1a89fe65a121c9464a73d773
Kevin Traynor <kevin@Kevins-MacBook-Pro-2.local>
2018-09-11 15:43:37 +1000

    Imported Images

--- src/middleware/images.js
+++ src/middleware/images.js
@@ -1,7 +1,31 @@
 //import abacusGreyLogo from '../assets/images/abacus-grey-logo.png';
 import fullLogo from '../assets/images/full-logo.png';
+import mediumLogo from '../assets/images/mediumlogo.png';
+import githubLogo from '../assets/images/githublogo.png';
+import redditLogo from '../assets/images/redditlogo.png';
+import twitterLogo from '../assets/images/twitterlogo.png';
+import telegramLogo from '../assets/images/telegramlogo.png';
+import iPhoneX from '../assets/images/iPhoneX.png';
+import buttonplay from '../assets/images/buttonplay.png';
+import netflixsubscription from '../assets/images/Subscription.png';
+import MKRLogo from '../assets/images/MKRlogo.png';
+import bitfwdLogo from '../assets/images/bitfwdlogo.png';
+import tenzLogo from '../assets/images/tenzlogo.png';
 
 export default {
   //abacusGreyLogo, 
   fullLogo,
+  mediumLogo,
+  githubLogo,
+  redditLogo,
+  twitterLogo,
+  telegramLogo,
+  iPhoneX,
+  buttonplay,
+  netflixsubscription,
+  MKRLogo,
+  bitfwdLogo,
+  tenzLogo
+
+
 };
